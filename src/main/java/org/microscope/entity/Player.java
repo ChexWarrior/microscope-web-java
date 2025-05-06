5dd6d3e65ff5bbe84d2aef7f9186b7ff70f598a2
ChexWarrior <aplehm@gmail.com>
2025-05-05 22:13:50 -0400

    Add player class

---
+++ src/main/java/org/microscope/entity/Player.java
@@ -0,0 +1,39 @@
+package org.microscope.entity;
+
+public class Player {
+    protected String email;
+
+    protected String username;
+
+    protected String password;
+
+    public Player(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
