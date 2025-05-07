484c6278dd8d53ece3151bb57eddf256903c942d
ChexWarrior <aplehm@gmail.com>
2025-05-06 20:53:43 -0400

    Display template for register page

--- src/main/java/org/microscope/controller/PlayerController.java
+++ src/main/java/org/microscope/controller/PlayerController.java
@@ -1,14 +1,14 @@
 package org.microscope.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class PlayerController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Hello World";
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register-player";
     }
 }
